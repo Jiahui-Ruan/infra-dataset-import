214fe44ef598a7c01e974fb7a879b087c49eb9d1
Jiahui Ruan <jr58@rice.edu>
2017-06-27 23:28:40 -0700

    finish read_cmd

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import webbrowser
 
+from subprocess import Popen
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, disconnect
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit, disconnect
+from handle_sh import FileReader
+import socket
 
 async_mode = None
 
@@ -14,6 +17,13 @@ socketio = SocketIO(app, async_mode=async_mode)
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
+
+fr = FileReader()
+for cmd in fr.get_all_cmd('bin/ds-rosbag-import.sh'): print cmd
+
+# @socketio.on('connect')
+# def test_connect():
+#     print "connect"
 
 if __name__ == '__main__':
     webbrowser.open('http://localhost:5005', autoraise=True)
