567fa7898b8c280eae89df3f747014fecfca7a42
Jiahui-Ruan <jr58@rice.edu>
2017-06-30 00:02:54 -0700

    finish ds-rosbag-scan

--- services/cmd_manager.py
+++ services/cmd_manager.py
@@ -1,10 +1,24 @@
-from config import socketio
-from subprocess import check_output
+import os
 
+from config import socketio
+from subprocess import Popen, PIPE
+
+pList = []
 
 @socketio.on('sendCmd')
 def handleCmd(cmdObj):
     cmd = cmdObj['cmd'] + cmdObj.get('param', '')
-    print cmd
-    out = check_output(cmd, shell=True)
-    print out
+    # check first if a cmd is cd
+    if cmd[:2] == 'cd':
+        os.chdir(cmd[3:])
+        print cmd[3:]
+        return
+
+    if not pList:
+        p = Popen(cmd, shell=True, stdout=PIPE)
+    else:
+        p = Popen(cmd, shell=True, stdout=PIPE, stdin=pList[-1].stdout)
+
+    pList.append(p)
+    stdout, err = p.communicate()
+    print stdout
