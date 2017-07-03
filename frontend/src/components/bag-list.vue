9d81634436dc5bf13e93c0af232870dfadbeaff9
Jiahui-Ruan <jr58@rice.edu>
2017-07-03 10:46:51 -0700

    use a compromized way to select bags

--- frontend/src/components/bag-list.vue
+++ frontend/src/components/bag-list.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="row">
-    <div class="twelve wide column">
+    <div class="twelve wide column baglist">
       <div class="ui segment">
         <div class="ui vertical segment">
           <paginate
@@ -9,7 +9,7 @@
             :per="20"
             tag="div"
             class="ui celled list">
-            <div v-for="bag in paginated('bags')" class="item" @click="toggleItem">
+            <div v-for="bag in paginated('bags')" class="item" @click="addBag">
               <i class="tasks icon"></i>
               {{ bag }}
             </div>
@@ -20,8 +20,18 @@
         </div>
       </div>
     </div>
-    <div class="four wide column">
+    <!-- <div class="four wide column">
       <textarea rows="48" cols="80" id="showBagArea"></textarea>
+    </div> -->
+    <div class="four wide column selectbag">
+      <div class="ui segment">
+        <div class="ui middle aligned divided list">
+          <div class="item" v-for="bag in selectBagObj['selectBag']">
+            {{ bag }}
+            <i class="remove icon" @click="removeBag(bag)"></i>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,13 +52,13 @@ export default {
     return {
       bagList: [],
       paginate: ['bags'],
-      selectBag: {
-        'select': []
+      selectBagObj: {
+        'selectBag': []
       }
     }
   },
   watch: {
-    'selectBag': {
+    'selectBagObj': {
       handler: function (obj) {
         var textedJSON = JSON.stringify(obj, replacer, 4)
         console.log(textedJSON)
@@ -58,15 +68,21 @@ export default {
     }
   },
   methods: {
-    toggleItem (event) {
+    addBag (event) {
       let target = event.target
-      if ($(target).hasClass('active')) {
-        $(target).removeClass('active')
-        // TODO: remember to delete the bag
-      } else {
-        $(target).addClass('active')
-        // cmdObj['param'] = event.data.param
-        this.selectBag['select'].push($(target).text())
+      let bag = $(target).text()
+      let array = this.selectBagObj['selectBag']
+      let idx = array.indexOf(bag)
+      if (idx > -1) {
+        return
+      }
+      array.push(bag)
+    },
+    removeBag (bag) {
+      let array = this.selectBagObj['selectBag']
+      let idx = array.indexOf(bag)
+      if (idx > -1) {
+        array.splice(idx, 1)
       }
     }
   },
@@ -79,10 +95,14 @@ export default {
 </script>
 
 <style lang="css">
+  .twelve.wide.column.baglist {
+    left: -20%;
+  }
+  .four.wide.column.selectbag {
+    left: -20%;
+  }
   div.ui.segment {
     width: 800px;
-    border: 0;
-    border: none;
   }
   div.ui.vertical.segment {
     border: 0;
@@ -90,9 +110,6 @@ export default {
   div.item {
     width: 750px;
     font-size: 25px;
-  }
-  div.item.active {
-    background-color: #1E90FF;
   }
   ul.paginate-links.bags {
     text-align: center;
